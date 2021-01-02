08085bec7131c1146fd062ecfbe80975f200924c
ebinayuuki <tyuuzen1@yahoo.co.jp>
2021-01-03 06:31:46 +0900

    messagesテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
+  has_many :messages
 
-  validates :name, presence: true
+  validates :name, presence: true #ユーザ登録時、nameキーにデータ未入力だとエラーを返す
 end
