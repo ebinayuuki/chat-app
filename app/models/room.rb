8718a1121950999967e04fdf9b9b5fb2e779c1dd
ebinayuuki <tyuuzen1@yahoo.co.jp>
2021-01-03 08:01:44 +0900

    チャットルームの削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
-  has_many :room_users
-  has_many :users, through: :room_users
-  has_many :messages
+  has_many :room_users, dependent: :destroy
+  has_many :users, through: :room_users  
+  has_many :messages, dependent: :destroy
 
   validates :name, presence: true #ユーザ登録時、nameキーにデータ未入力だとエラーを返す
 end
