7e6fad89ece314061baa2c455ff2042230853d80
ebinayuuki <tyuuzen1@yahoo.co.jp>
2021-01-03 03:10:46 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true #ユーザ登録時、nameキーにデータ未入力だとエラーを返す
+
 end
